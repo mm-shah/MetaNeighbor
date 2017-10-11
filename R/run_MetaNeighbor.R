edff9ec297066322b142402ab299c9022c52fc7f
mm-shah <manthan.ngu@gmail.com>
2017-10-11 13:29:43 -0400

    Added roxygen2 comments

--- R/run_MetaNeighbor.R
+++ R/run_MetaNeighbor.R
@@ -1,3 +1,40 @@
+#' Runs MetaNeighbor function.
+#'
+#' It builds a network of rank correlations between all cells for a gene set. Next,
+#' the neighbor voting predictor produces a weighted matrix of predicted labels by
+#' performing matrix multiplication between the network and the binary vector
+#' indicating cell type membership, then dividing each element by the null
+#' predictor (i.e., node degree). That is, each cell is given a score equal to the
+#' fraction of its neighbors (including itself), which are part of a given cell
+#' type. For cross-validation, we permute through all possible combinations of
+#' leave-one-dataset-out cross-validation, and we report how well we can recover
+#' cells of the same type as area under the receiver operator characteristic curve
+#' (AUROC). This is repeated for all folds of cross-validation, and the mean AUROC
+#' across folds is reported.
+#'
+#' @param data A gene-by-sample expression matrix.
+#' @param experiment_labels A numerical vector that indicates the source of each sample.
+#' @param celltype_labels A matrix that indicates the cell type of each sample.
+#' @param genesets Gene sets of interest provided as a list of vectors.
+#' @param file_ext Specify the desired path and file name for output file.
+#' @return There are three outputs of the method: i.) A vector of AUROC scores representing the mean for each gene set tested is
+#'     returned directly (see \code{\link{neighbor_voting_LeaveOneExpOut}}). ii.) A list containing the AUROC score for each dataset and cell type can be found
+#'     in the first output file. iii.) A matrix containing the means for each cell type across datasets can be found in the second output file.
+#'
+#' @keywords
+#'
+#' @examples
+#' load("MetaNeighbor_sample_data.Rdata")
+#' AUROC_scores = run_MetaNeighbor(data = data,
+#'                                 experiment_labels = exp_lab,
+#'                                 celltype_labels = cell_lab,
+#'                                 genesets = genesets,
+#'                                 file_ext = "filename")
+#' hist(AUROC_scores, main = "Sst Nos", xlab = "AUROC Scores", breaks = 20, xlim = c(0,1))
+#' abline(v = mean(AUROC_scores), col = "red", lty = 2, lwd = 2)
+#' @export
+#'
+
 run_MetaNeighbor <- function(data, experiment_labels, celltype_labels, genesets, file_ext) {
 
   ROCs              <- vector(mode = "list", length = length(genesets))
