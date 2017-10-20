2401cef9065af81dba89881026d3ec6db3b9741a
mm-shah <manthan.ngu@gmail.com>
2017-10-20 13:57:25 -0400

    Update neighbor_voting_LeaveOneExpOut.R

--- R/neighbor_voting_LeaveOneExpOut.R
+++ R/neighbor_voting_LeaveOneExpOut.R
@@ -1,92 +1,116 @@
-#' Runs neighbor voting algorithm.
+#' Runs the neighbor voting algorithm.
 #'
-#' It will perform certain fucntion
+#' The function performs cell type identity prediction based on 'guilt by
+#' association' using cross validation. Performance is evaluated by calculating
+#' the AUROC for each cell type.
 #'
-#' @param exp_labels A numerical vector that indicates the source of each sample.
-#' @param cell_labels A matrix that indicates the cell type of each sample.
-#' @param network Gene sets of interest provided as a list of vectors.
-#' @param means Defaut TRUE, mean of ROC values will be returned. If FLASE, list of ROC values will be returned
-#' @return aucs array of aucs for each group in annotations
+#' @param exp_labels A numerical vector that indicates the dataset source of
+#' each sample
+#' @param cell_labels A sample by cell type matrix that indicates the cell type
+#' of each sample (0-1)
+#' @param network A sample by sample adjacency matrix, ranked and standardized
+#' between 0-1
+#' @param means Default TRUE, determines output formatting
 #'
-#' @keywords neighbor voting, AUROC
+#' @return If \code{means = TRUE} (default) a vector containing the mean of
+#' AUROC values across cross-validation folds will be returned. If FALSE a list
+#' is returned containing a cell type by dataset matrix of AUROC scores, for
+#' each fold of cross-validation. Default is over-ridden when more than one cell
+#' type is assessed.
 #'
 #' @examples
+#' data(MetaNeighbor_sample_data.RData)
+#' AUROC_scores = run_MetaNeighbor(data = MetaNeighbor_sample_data$data,
+#'                                 experiment_labels = MetaNeighbor_sample_data$exp.lab,
+#'                                 celltype_labels = MetaNeighbor_sample_data$cell.lab,
+#'                                 genesets = MetaNeighbor_sample_data$genesets,
+#'                                 file_ext = "filename")
+#' AUROC_scores
 #'
 #' @export
 #'
 
-neighbor_voting_LeaveOneExpOut <- function( exp_labels, cell_labels, network, means=T){
+neighbor_voting_LeaveOneExpOut <- function (exp_labels,
+                                            cell_labels,
+                                            network,
+                                            means=TRUE){
 
-  # genes.label : needs to be in 1s and 0s
-  l <- dim(cell_labels)[2]
-  c <- dim(cell_labels)[1]
-  e <- unique(exp_labels)
+    # cell_labels : needs to be in 1s and 0s
+    l <- dim(cell_labels)[2]
+    c <- dim(cell_labels)[1]
+    e <- unique(exp_labels)
 
 
-  #print("Make genes label CV matrix")
-  test_cell_labels <- matrix(cell_labels, nrow=c, ncol = length(e)*l)
-  exp_cols <- rep(e, each = l)
+    #print("Make genes label CV matrix")
+    test_cell_labels <- matrix(cell_labels, nrow=c, ncol = length(e)*l)
+    exp_cols <- rep(e, each = l)
 
-  for (i in 1:length(e)){
-    d <- which(exp_labels == i)
-    a <- which(exp_cols == i)
-    test_cell_labels[d,a] <- 0
-  }
+    for (i in 1:length(e)){
+        d <- which(exp_labels == i)
+        a <- which(exp_cols == i)
+        test_cell_labels[d,a] <- 0
+    }
 
-  #print("Get sums - mat. mul.")
-  #sumin    = ( t(network) %*% test.genes.labels)
-  sum_in  <- (network %*% test_cell_labels)
+    #print("Get sums - mat. mul.")
+    sum_in  <- (network %*% test_cell_labels)
 
-  #print("Get sums - calc sumall")
-  sum_all <- matrix(apply(network, MARGIN = 2, FUN = sum), ncol = dim(sum_in)[2], nrow = dim(sum_in)[1])
+    #print("Get sums - calc sumall")
+    sum_all <- matrix(apply(network, MARGIN = 2, FUN = sum),
+                        ncol = dim(sum_in)[2],
+                        nrow = dim(sum_in)[1])
 
-  #print("Get sums - calc predicts")
-  predicts <- sum_in/sum_all
+    #print("Get sums - calc predicts")
+    predicts <- sum_in/sum_all
 
-  #print("Hide training data")
-  nans <- which(test_cell_labels == 1, arr.ind = T)
-  predicts[nans] <- NA
+    #print("Hide training data")
+    nans <- which(test_cell_labels == 1, arr.ind = TRUE)
+    predicts[nans] <- NA
 
-  #Hide other experiment data
-  for (i in 1:length(e)){
-    d <- which(exp_labels != i)
-    a <- which(exp_cols == i)
-    predicts[d,a] <- NA
-  }
+    #Hide other experiment data
+    for (i in 1:length(e)){
+        d <- which(exp_labels != i)
+        a <- which(exp_cols == i)
+        predicts[d,a] <- NA
+    }
 
-  #print("Rank test data")
-  predicts <- apply(abs(predicts), MARGIN = 2, FUN = rank, na.last = "keep", ties.method = "average")
-  filter <- matrix(cell_labels, nrow = c, ncol = length(e)*l)
+    #print("Rank test data")
+    predicts <- apply(abs(predicts),
+                        MARGIN = 2,
+                        FUN = rank,
+                        na.last = "keep",
+                        ties.method = "average")
+    filter <- matrix(cell_labels, nrow = c, ncol = length(e)*l)
 
-  for (i in 1:length(e)){
-    d <- which(exp_labels != i)
-    a <- which(exp_cols ==i )
-    filter[d,a] <- NA
-  }
+    for (i in 1:length(e)){
+        d <- which(exp_labels != i)
+        a <- which(exp_cols ==i )
+        filter[d,a] <- NA
+    }
 
-  negatives <- which(filter == 0, arr.ind = T)
-  positives <- which(filter == 1, arr.ind = T)
-  predicts[negatives] <- 0
+    negatives <- which(filter == 0, arr.ind = TRUE)
+    positives <- which(filter == 1, arr.ind = TRUE)
+    predicts[negatives] <- 0
 
-  #print("Calculate ROC - np")
-  np <- colSums(filter, na.rm = T) # Postives
+    #print("Calculate ROC - np")
+    np <- colSums(filter, na.rm = TRUE) # Postives
 
-  #print("Calculate ROC - nn")
-  nn <- apply(filter, MARGIN = 2, FUN = function(x) sum(x == 0, na.rm = T))     # Negatives
+    #print("Calculate ROC - nn")
+    nn <- apply(filter,
+                MARGIN = 2,
+                FUN = function(x) sum(x == 0, na.rm = TRUE))     # Negatives
 
-  #print("Calculate ROC - p")
-  p  <- apply(predicts, MARGIN = 2, FUN = sum, na.rm = T)
+    #print("Calculate ROC - p")
+    p  <- apply(predicts, MARGIN = 2, FUN = sum, na.rm = TRUE)
 
-  #print("Calculate ROC - rocN")
-  rocNV           <- (p/np - (np+1)/2)/nn
-  rocNV           <- matrix(rocNV, ncol = length(e), nrow = l)
-  colnames(rocNV) <- e
-  rownames(rocNV) <- colnames(cell_labels)
+    #print("Calculate ROC - rocN")
+    rocNV           <- (p/np - (np+1)/2)/nn
+    rocNV           <- matrix(rocNV, ncol = length(e), nrow = l)
+    colnames(rocNV) <- e
+    rownames(rocNV) <- colnames(cell_labels)
 
-  if(means==T){
-    scores = list(rowMeans(rocNV ,na.rm = T))
-  } else {
-    scores = list(rocNV)
-  }
+    if(means==TRUE){
+        scores = list(rowMeans(rocNV ,na.rm = TRUE))
+    } else {
+        scores = list(rocNV)
+    }
 }
-
